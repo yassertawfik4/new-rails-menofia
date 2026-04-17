c62b879b0965e8168d0bb8ed7b12da3193af70d2
yassertawfik4 <yassertito682@gmail.com>
2026-04-17 15:02:40 +0200

    feat: implement CRUD actions for posts in PostsController and update schema version

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,43 @@
 class PostsController < ApplicationController
   # your code goes here
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
+  # GET /posts/:id
+  def show
+    @post = Post.find(params[:id])
+  end
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+  # POST /posts
+  def create
+    @post=Post.new(title: params[:post][:title], content: params[:post][:content])
+    if @post.save
+      redirect_to @post, notice: "Post was created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  # GET /posts/:id/edit
+  def edit
+    @post = Post.find(params[:id])
+  end
+  # PATCH/PUT /posts/:id
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(title: params[:post][:title], content: params[:post][:content])
+      redirect_to @post, notice: "Post was updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  # DELETE /posts/:id
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "Post was deleted."
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_17_105419) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_17_105853) do
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
