29ec8993cfda34c2a15011bfe413b02d232c6759
yassertawfik4 <yassertito682@gmail.com>
2026-04-18 14:20:46 +0200

    feat: add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,44 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+PostEditor.destroy_all
+Post.destroy_all
+Editor.destroy_all
+User.destroy_all
+
+user1 = User.create!(
+  name: "Ahmed",
+  age: 30
+)
+
+user2 = User.create!(
+  name: "Sara",
+  age: 28
+)
+
+editor1 = Editor.create!(
+  name: "Mona",
+  email: "mona@example.com"
+)
+
+editor2 = Editor.create!(
+  name: "Omar",
+  email: "omar@example.com"
+)
+
+post1 = Post.create!(
+  title: "First Post",
+  user_id: user1.id
+)
+
+post2 = Post.create!(
+  title: "Second Post",
+  user_id: user2.id
+)
+
+PostEditor.create!(post: post1, editor: editor1)
+PostEditor.create!(post: post1, editor: editor2)
+PostEditor.create!(post: post2, editor: editor2)
+
+puts "Seed data created successfully!"
